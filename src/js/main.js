dd95306169358dd6ffa9f693cc9a32af5870342a
DenormeLukas <lukas.denorme@student.howest.be>
2022-10-18 19:15:45 +0200

    Added interaction with buttons, not working 100%

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,20 @@ import portalFragmentShader3 from '../shaders/portal/fragment3.glsl?raw';
 
 {
 
-    let filter = 2;
+    const cursor = document.querySelector('.cursor');
+
+
+    const moveCursor = (e) => {
+        const mouseY = e.clientY;
+        const mouseX = e.clientX;
+
+        cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+    }
+
+    window.addEventListener('mousemove', moveCursor);
+
+    let filter = 1;
 
     const canvas = document.querySelector('canvas.webgl');
     const scene = new THREE.Scene();
@@ -41,16 +54,15 @@ import portalFragmentShader3 from '../shaders/portal/fragment3.glsl?raw';
     renderer.setSize(size.width, size.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    const uniforms = {
-        iTime: { value: 0 },
-        iResolution: { value: new THREE.Vector3(10, 10, 0) },
-    };
 
     const materialShader = new THREE.ShaderMaterial({
 
         fragmentShader: portalFragmentShader,
         vertexShader: portalVertexShader,
-        uniforms,
+        uniforms: {
+            iTime: { value: 0 },
+            iResolution: { value: new THREE.Vector3(10, 10, 0) },
+        }
 
     });
 
@@ -94,50 +106,89 @@ import portalFragmentShader3 from '../shaders/portal/fragment3.glsl?raw';
     pointLight2.position.set(-10, 4, -1);
     scene.add(pointLight2);
 
+    const loadShaders = () => {
 
-    const loader2 = new GLTFLoader();
+        const loader2 = new GLTFLoader();
 
-    loader2.load(
-        // resource URL
-        'assets/scene.glb',
-        (gltf) => {
-            gltf.scene.traverse(child => {
+        loader2.load(
+            // resource URL
+            'assets/scene.glb',
+            (gltf) => {
+                gltf.scene.traverse(child => {
 
-                if (child.name === "Object_3") {
-                    switch (filter) {
-                        case 1:
-                            child.material = materialShader;
-                            break;
-                        case 2:
-                            child.material = materialShader2;
-                            break;
-                        case 3:
-                            child.material = materialShader3;
-                            break;
-                        default:
-                            break;
+                    if (child.name === "Object_3") {
+                        switch (filter) {
+                            case 1:
+                                child.material = materialShader;
+                                break;
+                            case 2:
+                                child.material = materialShader2;
+                                break;
+                            case 3:
+                                child.material = materialShader3;
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                }
 
-            })
-            scene.add(gltf.scene);
-        }
-    );
+                })
+                scene.add(gltf.scene);
+            }
+        );
+
+    }
 
     const clock = new THREE.Clock();
 
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+
+    function onPointerMove() {
+
+        pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster.setFromCamera(pointer, camera);
+
+        const intersects = raycaster.intersectObjects(scene.children, true);
+
+        console.log(intersects[0].object.name)
+
+        if (intersects[0].object.name === "Object_16" || intersects[0].object.name === "Object_13") {
+
+            filter = 1;
+
+        } else if (intersects[0].object.name === "Object_8" || intersects[0].object.name === "Object_14") {
+
+            filter = 2;
+
+        } else if (intersects[0].object.name === "Object_2" || intersects[0].object.name === "Object_15") {
+
+            filter = 3;
+
+        }
+
+        console.log(filter);
+        loadShaders();
+
+
+    }
+
     const draw = () => {
+
 
         const elapsedTime = clock.getElapsedTime();
         materialShader.uniforms.iTime.value = elapsedTime;
         materialShader2.uniforms.iTime.value = elapsedTime;
         materialShader3.uniforms.iTime.value = elapsedTime;
 
+        window.addEventListener('mousedown', onPointerMove);
+
         renderer.render(scene, camera);
         window.requestAnimationFrame(draw);
 
     }
-
 
     window.addEventListener('resize', () => {
 
@@ -157,6 +208,7 @@ import portalFragmentShader3 from '../shaders/portal/fragment3.glsl?raw';
 
     })
 
+    loadShaders();
     draw();
 
 }
